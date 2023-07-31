ce4ffe0da876c652a7760146cbfc583de67faa12
User <ol.shink87@gmail.com>
2023-07-31 17:28:38 +0300

    fixed

--- src/Employee.java
+++ src/Employee.java
@@ -6,7 +6,6 @@ public class Employee {
     private int department;
     private int salary;
     private int id;
-    private static ArrayList<Employee> employees = new ArrayList<>();
     private static int count = 1;
 
     public Employee(String name, int department, int salary) {
@@ -14,19 +13,8 @@ public class Employee {
         this.department = department;
         this.salary = salary;
         this.id = count;
-        employees.add(this);//?
         count++;
     }
-
-    // Additional method to remove an employee from the list
-    public static void removeEmployee(Employee employee) {
-        employees.remove(employee);
-    }
-
-    public static ArrayList<Employee> getEmployees() {
-        return employees;
-    }
-
     public String getName() {
         return this.name;
     }
--- src/Main.java
+++ src/Main.java
@@ -2,144 +2,168 @@ import java.util.ArrayList;
 
 public class Main {
     //для вывода списка всех сотрудников
-    public static void printEmpl(ArrayList<Employee> obj) {
-        for (int i = 0; i < obj.size(); i++) {
-            System.out.println(obj.get(i));
-            System.out.println("=======================================");
+    public static void printEmpl(Employee[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
+                System.out.println(arr[i]);
+                System.out.println("=======================================");
+            }
         }
     }
 
     //для вывода суммы зарплат всех сотрудников
-    public static float sumSalary(ArrayList<Employee> obj) {
+    public static float sumSalary(Employee[] arr) {
         int sum = 0;
         for (Employee a :
-                obj) {
-            sum = sum + a.getSalary();
+                arr) {
+            if (a != null) {
+                sum = sum + a.getSalary();
+            }
         }
         return sum;
     }
 
     // для вывода сотрудника с минимальной ЗП
-    public static Employee minSalary(ArrayList<Employee> obj) {
+    public static Employee minSalary(Employee[] arr) {
         int index = 0;
-        int min = obj.get(0).getSalary();
-        for (int i = 0; i < obj.size(); i++) {
-            if (min > obj.get(i).getSalary()) {
-                min = obj.get(i).getSalary();
-                index = i;
+        int min = arr[0].getSalary();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
+                if (min > arr[i].getSalary()) {
+                    min = arr[i].getSalary();
+                    index = i;
+                }
             }
         }
 
-        return obj.get(index);
+        return arr[index];
     }
 
     // для вывода сотрудника с максиимальной ЗП
-    public static Employee maxSalary(ArrayList<Employee> obj) {
+    public static Employee maxSalary(Employee[] arr) {
         int ind = 0;
-        int max = obj.get(0).getSalary();
-        for (int i = 0; i < obj.size(); i++) {
-            if (max < obj.get(i).getSalary()) {
-                max = obj.get(i).getSalary();
-                ind = i;
+        int max = arr[0].getSalary();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
+                if (max < arr[i].getSalary()) {
+                    max = arr[i].getSalary();
+                    ind = i;
+                }
             }
         }
-        return obj.get(ind);
+        return arr[ind];
     }
 
     //для вывода средней ЗП
-    public static int averageSalary(ArrayList<Employee> obj) {
+    public static int averageSalary(Employee[] arr) {
         int sum = 0;
         int count = 0;
         for (Employee a :
-                obj) {
-            sum = sum + a.getSalary();
-            count++;
+                arr) {
+            if (a != null) {
+                sum = sum + a.getSalary();
+                count++;
+            }
         }
         return sum / count;
     }
 
     //для вывода имен всех сотрудников
-    public static void printNameOfEmployees(ArrayList<Employee> obj) {
-        for (Employee o : obj
+    public static void printNameOfEmployees(Employee[] arr) {
+        for (Employee o : arr
         ) {
-            System.out.println("ФИО сотрудника = " + o.getName());
+            if (o != null) {
+                System.out.println("ФИО сотрудника = " + o.getName());
+            }
         }
     }
 
     //для индексирования ЗП
-    public static void indexSalaryByPercentage(ArrayList<Employee> employees, int percent) {
-        for (Employee o : employees) {
-            int currentSalary = o.getSalary();
-            int indexSalary = (currentSalary + (currentSalary / 100 * percent));
-            o.setSalary(indexSalary);
+    public static void indexSalaryByPercentage(Employee[] arr, int percent) {
+        for (Employee o : arr) {
+            if (o != null) {
+                int currentSalary = o.getSalary();
+                int indexSalary = (currentSalary + (currentSalary / 100 * percent));
+                o.setSalary(indexSalary);
+            }
         }
     }
 
     //нати сотрудника с мин ЗП по номеру отдела
-    public static Employee findMinSalaryByDepartment(ArrayList<Employee> employees, int numberOfDepartment) {
+    public static Employee findMinSalaryByDepartment(Employee[] arr, int numberOfDepartment) {
         int index = 0;
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i < employees.size(); i++) {
-            if (employees.get(i).getDepartment() == numberOfDepartment && employees.get(i).getSalary() < min) {
-                min = employees.get(i).getSalary();
-                index = i;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
+                if (arr[i].getDepartment() == numberOfDepartment && arr[i].getSalary() < min) {
+                    min = arr[i].getSalary();
+                    index = i;
+                }
             }
         }
-        return employees.get(index);
+        return arr[index];
     }
 
     //нати сотрудника с max ЗП по номеру отдела
-    public static Employee findMaxSalaryByDepartment(ArrayList<Employee> employees, int numberOfDepartment) {
+    public static Employee findMaxSalaryByDepartment(Employee[] arr, int numberOfDepartment) {
         Employee employee = null;
         int max = -1;
-        for (Employee o : employees) {
-            if (o.getDepartment() == numberOfDepartment && o.getSalary() > max) {
-                max = o.getSalary();
-                employee = o;
+        for (Employee o : arr) {
+            if (o != null) {
+                if (o.getDepartment() == numberOfDepartment && o.getSalary() > max) {
+                    max = o.getSalary();
+                    employee = o;
+                }
             }
         }
         return employee;
     }
 
     //сумма затрат на ЗП по отделу
-    public static int getTotalSalaryCostByDepartment(ArrayList<Employee> employees, int department) {
+    public static int getTotalSalaryCostByDepartment(Employee[] arr, int department) {
         int totalCost = 0;
         for (Employee o :
-                employees) {
-            if (o.getDepartment() == department) {
-                totalCost += o.getSalary();
+                arr) {
+            if (o != null) {
+                if (o.getDepartment() == department) {
+                    totalCost += o.getSalary();
+                }
             }
         }
         return totalCost;
     }
 
     //средняя ЗП по отделу
-    public static int getAverageSalaryCostByDepartment(ArrayList<Employee> employees, int department) {
+    public static int getAverageSalaryCostByDepartment(Employee[] arr, int department) {
         int averageCost = 0;
         int count = 0;
         for (Employee o :
-                employees) {
-            if (o.getDepartment() == department) {
-                averageCost += o.getSalary();
-                count++;
+                arr) {
+            if (o != null) {
+                if (o.getDepartment() == department) {
+                    averageCost += o.getSalary();
+                    count++;
+                }
             }
         }
         return averageCost / count;
     }
 
     //индексация ЗП сотрудников одного отдела
-    public static void indexSalaryCostByDepartment(ArrayList<Employee> employees, int department, int percent) {
-        for (Employee o : employees) {
-            if (o.getDepartment() == department) {
-                int currentSalary = o.getSalary();
-                int indexSalary = currentSalary + (currentSalary / 100 * percent);
-                o.setSalary(indexSalary);
+    public static void indexSalaryCostByDepartment(Employee[] arr, int department, int percent) {
+        for (Employee o : arr) {
+            if (o != null) {
+                if (o.getDepartment() == department) {
+                    int currentSalary = o.getSalary();
+                    int indexSalary = currentSalary + (currentSalary / 100 * percent);
+                    o.setSalary(indexSalary);
+                }
             }
         }
     }
 
     //напечатать всех сотрудников отдела - все данные, кроме отдела
-    public static void printEmployeesInDepartment(ArrayList<Employee> employees, int department) {
+    public static void printEmployeesInDepartment(Employee[] employees, int department) {
         for (Employee o : employees) {
             if (o.getDepartment() == department) {
                 System.out.println(o.getName() + " " + o.getSalary() + " " + o.getId() +
@@ -148,29 +172,25 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
 
-        Employee ivanov = new Employee("Иванов Иван Иванович", 1, 1000);
-        Employee tumanov = new Employee("Туманов Алексей Сергеевич", 1, 200);
-        Employee petrov = new Employee("Петров Петр Петрович", 2, 1200);
-        Employee sidorov = new Employee("Сидоров Сидр Сидорович", 3, 125);
-        Employee kuznetsov = new Employee("Кузнецов Илларион Феофилович", 4, 1500);
-        Employee kovalev = new Employee("Ковалев Владимир Красносолнышко", 5, 1_000_000);
-      printEmployeesInDepartment(Employee.getEmployees(), 1);
-        /*  System.out.println(findMinSalaryByDepartment(Employee.getEmployees(), 1));
-        System.out.println(findMaxSalaryByDepartment(Employee.getEmployees(), 1));
-        System.out.println(getTotalSalaryCostByDepartment(Employee.getEmployees(), 1));
-        System.out.println(getAverageSalaryCostByDepartment(Employee.getEmployees(), 1));
-        indexSalaryCostByDepartment(Employee.getEmployees(), 1, 15);
-        System.out.println(getTotalSalaryCostByDepartment(Employee.getEmployees(), 1
-        ));
-        printEmpl(Employee.getEmployees());
-        System.out.println("Сумма затрат на зарплату в месяц - " + sumSalary(Employee.getEmployees()) + '\u20AC' + "\n");
-        System.out.println("Наименьшая ЗП у сотрудника - \n" + minSalary(Employee.getEmployees()) + "\n");
-        System.out.println("Наибольшая ЗП у сотрудника - \n" + maxSalary(Employee.getEmployees()) + "\n");
-        System.out.println("Среднее значение зарплат = " + averageSalary(Employee.getEmployees()) + "\n");
-        printNameOfEmployees(Employee.getEmployees());
-        indexSalaryByPercentage(Employee.getEmployees(), 10);
-        printEmpl(Employee.getEmployees());*/
+    public static void main(String[] args) {
+        Employee[] employeesList = new Employee[10];
+        int employeeCount = 0;
+
+        employeesList[employeeCount++] = new Employee("Иванов Иван Иванович", 1, 1000);
+        employeesList[employeeCount++] = new Employee("Туманов Алексей Сергеевич", 1, 200);
+        employeesList[employeeCount++] = new Employee("Петров Петр Петрович", 2, 1200);
+        employeesList[employeeCount++] = new Employee("Сидоров Сидр Сидорович", 3, 125);
+        employeesList[employeeCount++] = new Employee("Кузнецов Илларион Феофилович", 4, 1500);
+        employeesList[employeeCount++] = new Employee("Ковалев Владимир Красносолнышко", 5, 1_000_000);
+
+        indexSalaryByPercentage(employeesList, 20);
+
+        printEmpl(employeesList);
+        System.out.println(sumSalary(employeesList));
+        System.out.println("minSalary(employeesList) = " + minSalary(employeesList));
+        System.out.println("maxSalary(employeesList) = " + maxSalary(employeesList));
+        printNameOfEmployees(employeesList);
+        System.out.println(findMinSalaryByDepartment(employeesList, 1));
     }
 }
